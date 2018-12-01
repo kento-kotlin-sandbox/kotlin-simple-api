7392747778cbf6419a63597a75b96de4cea96bca
Kento75 <kento2github@gmail.com>
2018-12-02 07:35:05 +0900

    Add rest routes

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # kotlin-simple-api
+
+Java ver 11
+Kotlin ver 1.3
+Spring boot 2.1
--- simpleapi/src/main/kotlin/com/example/simpleapi/controller/SimpleController.kt
+++ simpleapi/src/main/kotlin/com/example/simpleapi/controller/SimpleController.kt
@@ -1,9 +1,7 @@
 package com.example.simpleapi.controller
 
 import com.example.simpleapi.data.Message
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -12,15 +10,38 @@ import java.util.*
 @RequestMapping("/simple")
 class SimpleController {
 
-    @GetMapping
+    @GetMapping(value = ["/display"])
     fun getMessages() : List<Message> {
         return listOf(
                 Message(
                         UUID.randomUUID().toString(),
                         "First Message",
                         "This is a 1st message on ${getDate()}."
+                ),
+                Message(
+                        UUID.randomUUID().toString(),
+                        "second Message",
+                        "this is a 2nd message on ${getDate()}."
                 )
         )
+    }
+
+    @PutMapping(value = ["/insert"])
+    fun insertMessage(@RequestBody message: Message) : Message {
+        message.id = UUID.randomUUID().toString()
+        return message
+    }
+
+    @PostMapping(value = ["/update"])
+    fun updateMessage(@RequestBody message: Message) : Message {
+        message.title += "UPDATED TITLE:${getDate()}"
+        message.message += "UPDATED MESSAGE:${getDate()}"
+        return message
+    }
+
+    @DeleteMapping(value = ["/delete/{id}"])
+    fun deleteMessage(@PathVariable(name = "id") id: String): Boolean {
+        return true
     }
 
     private fun getDate() : String {
